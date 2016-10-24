e9a655f7e2694952838bdca43d046da2f0c4dbe4
Timo Savola <timo.savola@iki.fi>
2016-10-25 02:17:23 +0300

    run: death signal

--- run/run.go
+++ run/run.go
@@ -281,6 +281,7 @@ func Run(env *Environment, payload *Payload) (output []byte, err error) {
 			env.loader,
 		},
 		SysProcAttr: &syscall.SysProcAttr{
+			Pdeathsig:  syscall.SIGKILL,
 			Cloneflags: syscall.CLONE_NEWUSER,
 		},
 	}
