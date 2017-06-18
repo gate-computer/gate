ddb8c83fc50d9eea4b8bdb47a5ea0047fd40ecaf
Timo Savola <timo.savola@iki.fi>
2017-06-19 01:34:02 +0300

    service: origin: implement input

--- service/origin/origin.go
+++ service/origin/origin.go
@@ -11,6 +11,8 @@ const (
 	Version = 0
 
 	packetHeaderSize = 8
+
+	maxPacketSize = 0x10000 // TODO: move this elsewhere
 )
 
 type Factory struct {
@@ -23,7 +25,7 @@ func (f *Factory) Register(r *service.Registry) {
 }
 
 func (f *Factory) New() service.Instance {
-	return &origin{f.R, f.W}
+	return &origin{r: f.R, w: f.W}
 }
 
 var Default = new(Factory)
@@ -41,9 +43,16 @@ func CloneRegistryWith(r *service.Registry, origIn io.Reader, origOut io.Writer)
 type origin struct {
 	r io.Reader
 	w io.Writer
+
+	reading chan struct{}
 }
 
 func (o *origin) Handle(buf []byte, replies chan<- []byte) {
+	if o.r != nil && o.reading == nil {
+		o.reading = make(chan struct{})
+		go o.readLoop(buf[6:8], replies)
+	}
+
 	if o.w != nil {
 		if _, err := o.w.Write(buf[packetHeaderSize:]); err != nil {
 			// assume that the error is EOF, broken pipe or such
@@ -53,4 +62,28 @@ func (o *origin) Handle(buf []byte, replies chan<- []byte) {
 }
 
 func (o *origin) Shutdown() {
+	if o.reading != nil {
+		close(o.reading)
+	}
+}
+
+func (o *origin) readLoop(code []byte, replies chan<- []byte) {
+	for {
+		buf := make([]byte, maxPacketSize) // TODO: smaller buffer?
+		copy(buf[6:8], code)
+
+		n, err := o.r.Read(buf[packetHeaderSize:])
+		if err != nil {
+			o.r = nil
+			return
+		}
+
+		select {
+		case replies <- buf[:packetHeaderSize+n]:
+			// ok
+
+		case <-o.reading:
+			return
+		}
+	}
 }
