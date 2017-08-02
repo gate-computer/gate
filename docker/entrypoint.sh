00535c3fcd80caf0fe1df235ac264c07a6bca2af
Timo Savola <timo.savola@iki.fi>
2017-08-02 07:44:23 +0300

    docker: fixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 common_gid=$(id -g uucp)
 
-exec sudo -E -n -u daemon -- /usr/bin/gate-server -common-gid=${pipe_gid} -container-uid=900 -container-gid=900 -executor-uid=901 -executor-gid=901 -libdir=/usr/lib/gate "$@"
+exec sudo -E -n -u daemon -- /usr/bin/gate-server -common-gid=${common_gid} -container-uid=900 -container-gid=900 -executor-uid=901 -executor-gid=901 -libdir=/usr/lib/gate -addr=:8888 "$@"
