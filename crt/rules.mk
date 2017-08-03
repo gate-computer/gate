b5b51452c9c5146589547288875753cb39cc822d
Timo Savola <timo.savola@iki.fi>
2017-08-04 00:09:28 +0300

    build wasm files in Docker
    Setting TOOLCHAINDIR avoids Docker.

--- crt/rules.mk
+++ crt/rules.mk
@@ -1,8 +1,34 @@
-WAGTOOLCHAIN	?= $(GATEDIR)/../wag-toolchain
+ifeq ($(TOOLCHAINDIR),)
+DOCKER		?= docker
 
-CC		:= $(WAGTOOLCHAIN)/bin/compile
-CXX		:= $(WAGTOOLCHAIN)/bin/compile
-LINKER		:= $(WAGTOOLCHAIN)/bin/link
+TOOLCHAINIMAGE	?= tsavola/wag-toolchain
+
+uid		:= $(shell id -u)
+gid		:= $(shell id -g)
+gatedir		:= $(shell realpath $(GATEDIR))
+pwd		:= $(shell pwd)
+dockertoolchain	:= $(DOCKER) run --rm -i -u $(uid):$(gid) -v $(gatedir):$(gatedir) -w $(pwd) -e PYTHONPATH=$$(echo $$PYTHONPATH) -e WAGTOOLCHAIN_ALLOCATE_STACK=$$(echo $$WAGTOOLCHAIN_ALLOCATE_STACK) $(TOOLCHAINIMAGE)
+
+# directories are inside Docker container
+LLVMBINDIR	:= /usr/local/llvm-build/bin
+BINARYENBINDIR	:= /usr/local/binaryen-build/bin
+
+PYTHON		:= $(dockertoolchain) python
+CC		:= $(dockertoolchain) compile
+CXX		:= $(dockertoolchain) compile
+LINKER		:= $(dockertoolchain) link
+LLVMAS		:= $(dockertoolchain) $(LLVMBINDIR)/llvm-as
+else
+PYTHON		?= python
+
+LLVMBINDIR	:= $(TOOLCHAINDIR)/llvm-build/bin
+BINARYENBINDIR	:= $(TOOLCHAINDIR)/binaryen-build/bin
+
+CC		:= $(TOOLCHAINDIR)/bin/compile
+CXX		:= $(TOOLCHAINDIR)/bin/compile
+LINKER		:= $(TOOLCHAINDIR)/bin/link
+LLVMAS		:= $(LLVMBINDIR)/llvm-as
+endif
 
 CPPFLAGS	+= -isystem $(GATEDIR)/include
 
