6a5894b5a42318a113501ec93f38259ebea2e190
Timo Savola <timo.savola@iki.fi>
2017-10-04 00:09:25 +0300

    libc: update musl (WebAssembly fork)
    Although it doesn't contain any relevant changes.

--- libc/musl.py
+++ libc/musl.py
@@ -2,16 +2,19 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import sys
+
 import libc
 
-
 libc.WARNINGS += [
-	"-Wno-absolute-value",         # src/math/fabsl.c:5
-	"-Wno-logical-op-parentheses", # src/internal/shgetc.c:16
-	"-Wno-macro-redefined",
+    "-Wno-absolute-value",
+    "-Wno-logical-op-parentheses",
+    "-Wno-macro-redefined",
 ]
 
 
 if __name__ == "__main__":
-	args = libc.getargs()
-	exit(libc.run(args.clang_dir, args.binaryen_dir, args.sexpr_wasm, args.musl, args.arch, args.out, args.save_temps))
+    args = libc.getargs()
+    if args.verbose:
+        libc.verbose = True
+    sys.exit(libc.run(args.clang_dir, args.binaryen_dir, args.sexpr_wasm, args.musl, args.arch, args.out, args.save_temps, args.compile_to_wasm))
