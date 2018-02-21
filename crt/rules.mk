b7e8688e1a382eb42b5093ddf6e36c3b758e6c45
Timo Savola <timo.savola@iki.fi>
2018-02-21 03:15:31 +0200

    don't build wasm files in Docker
    wag-toolchain needs to be built, and LLVMPREFIX needs to point to
    <wag-toolchain>/out.

--- crt/rules.mk
+++ crt/rules.mk
@@ -2,47 +2,20 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-ifeq ($(TOOLCHAINDIR),)
-DOCKER		?= docker
+LLVMPREFIX	?= $(GATEDIR)/../wag-toolchain/out
 
-TOOLCHAINIMAGE	?= tsavola/wag-toolchain
-
-uid		:= $(shell id -u)
-gid		:= $(shell id -g)
-gatedir		:= $(shell realpath $(GATEDIR))
-pwd		:= $(shell pwd)
-dockertoolchain	:= $(DOCKER) run --rm -i -u $(uid):$(gid) -v $(gatedir):$(gatedir) -w $(pwd) -e PYTHONPATH=$$(echo $$PYTHONPATH) -e WAGTOOLCHAIN_ALLOCATE_STACK=$$(echo $$WAGTOOLCHAIN_ALLOCATE_STACK) $(TOOLCHAINIMAGE)
-
-# directories are inside Docker container
-LLVMBINDIR	:= /usr/local/llvm-build/bin
-BINARYENBINDIR	:= /usr/local/binaryen-build/bin
-
-PYTHON		:= $(dockertoolchain) python
-CC		:= $(dockertoolchain) compile
-CXX		:= $(dockertoolchain) compile
-LINKER		:= $(dockertoolchain) link
-LLVMAS		:= $(dockertoolchain) $(LLVMBINDIR)/llvm-as
-LLVMLINK	:= $(dockertoolchain) $(LLVMBINDIR)/llvm-link
-else
-PYTHON		?= python
-
-LLVMBINDIR	:= $(TOOLCHAINDIR)/llvm-build/bin
-BINARYENBINDIR	:= $(TOOLCHAINDIR)/binaryen-build/bin
-
-CC		:= $(TOOLCHAINDIR)/bin/compile
-CXX		:= $(TOOLCHAINDIR)/bin/compile
-LINKER		:= $(TOOLCHAINDIR)/bin/link
-LLVMAS		:= $(LLVMBINDIR)/llvm-as
-LLVMLINK	:= $(LLVMBINDIR)/llvm-link
-endif
+CC		:= $(LLVMPREFIX)/bin/clang
+CXX		:= $(LLVMPREFIX)/bin/clang
+LLVMAS		:= $(LLVMPREFIX)/bin/llvm-as
+LLVMLINK	:= $(LLVMPREFIX)/bin/llvm-link
 
 CPPFLAGS	+= -isystem $(GATEDIR)/capi/include
 
 prog.wasm: $(OBJECTS)
-	$(LINKER) -o $@ $(OBJECTS) $(LIBS)
+	$(CC) -nostdlib -Wl,--allow-undefined-file=$(GATEDIR)/capi/abi.list -Wl,--check-signatures $(CFLAGS) $(LDFLAGS) -o $@ $(GATEDIR)/crt/crt.bc $(OBJECTS) $(LIBS)
 
 %.bc: %.c
-	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $*.c
+	$(CC) --target=wasm32-unknown-unknown -emit-llvm $(CPPFLAGS) $(CFLAGS) -c -o $@ $*.c
 
 %.bc: %.cpp
-	$(CXX) $(CPPFLAGS) $(CFLAGS) $(CXXFLAGS) -D_LIBCPP_HAS_MUSL_LIBC -D_LIBCPP_HAS_NO_THREADS -D__ELF__ -isystem $(GATEDIR)/libcxx/libcxx/include -include $(GATEDIR)/crt/main.hpp -fno-exceptions -c -o $@ $*.cpp
+	$(CXX) --target=wasm32-unknown-unknown -emit-llvm $(CPPFLAGS) $(CFLAGS) $(CXXFLAGS) -D_LIBCPP_HAS_MUSL_LIBC -D_LIBCPP_HAS_NO_THREADS -D__ELF__ -isystem $(GATEDIR)/libcxx/libcxx/include -include $(GATEDIR)/crt/main.hpp -fno-exceptions -c -o $@ $*.cpp
