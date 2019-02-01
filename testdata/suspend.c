e0b709a5ddb7abf866a5d2e8a8255799ee013ae6
Timo Savola <timo.savola@iki.fi>
2019-02-01 19:27:40 +0200

    testdata: suspend: print numbers slowly

--- testdata/suspend.c
+++ testdata/suspend.c
@@ -2,8 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#include <gate.h>
+
+static void slow_nop(void)
+{
+	gate_io(NULL, NULL, NULL, NULL, 0);
+}
+
+static void delay(void)
+{
+	for (int i = 0; i < 10000000; i++)
+		slow_nop();
+}
+
+static void iteration(long i)
+{
+	gate_debug2(i, "\n");
+	delay();
+}
+
 int main(void)
 {
-	while (1)
-		;
+	for (long i = 0;; i++)
+		iteration(i);
 }
