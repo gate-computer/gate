1f3b7eeea69de1d1655c10d3da94323f9061f0b7
Timo Savola <timo.savola@iki.fi>
2019-10-09 22:30:55 +0300

    internal/debug package

--- internal/debug/debug.go
+++ internal/debug/debug.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build debug gateruntimedebug
+// +build gatedebug
 
-package runtime
+package debug
 
 import (
 	"fmt"
 )
 
-func debugf(format string, args ...interface{}) {
-	print(fmt.Sprintf("runtime/"+format+"\n", args...))
+func Printf(format string, args ...interface{}) {
+	print(fmt.Sprintf(format+"\n", args...))
 }
--- internal/debug/nodebug.go
+++ internal/debug/nodebug.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !debug,!gateruntimedebug
+// +build !gatedebug
 
-package runtime
+package debug
 
-func debugf(format string, args ...interface{}) {}
+func Printf(format string, args ...interface{}) {}
