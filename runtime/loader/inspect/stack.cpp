fbca321436eb84c51e0948e19bab03aee6d3366d
Timo Savola <timo.savola@iki.fi>
2022-01-18 00:53:40 +0200

    runtime/loader/inspect: use anonymous namespace

--- runtime/loader/inspect/stack.cpp
+++ runtime/loader/inspect/stack.cpp
@@ -13,64 +13,56 @@
 #include "attribute.hpp"
 #include "syscall.hpp"
 
-#define SYS_SA_RESTORER 0x04000000
+using runtime::syscall;
 
-struct sys_sigaction {
+namespace {
+
+struct Sigaction {
 	void (*handler)(int);
 	int flags;
 	void (*restorer)(void);
 	sigset_t mask;
 };
 
-extern "C" void* memset(void* s, int c, size_t n)
-{
-	for (size_t i = 0; i < n; i++)
-		reinterpret_cast<uint8_t*>(s)[i] = c;
-	return s;
-}
+const int sa_restorer = 0x04000000;
 
-NORETURN
-static void sys_exit(int status)
+NORETURN void sys_exit(int status)
 {
 	syscall1(SYS_exit, status);
 	__builtin_unreachable();
 }
 
-static int sys_fork()
+int sys_fork()
 {
 	return syscall6(SYS_clone, SIGCHLD, 0, 0, 0, 0, 0);
 }
 
-static int sys_sigaction(
-	int signum,
-	const struct sys_sigaction* act,
-	struct sys_sigaction* oldact)
+int sys_sigaction(int signum, const Sigaction* act, Sigaction* oldact)
 {
 	size_t masksize = 8;
 	return syscall4(SYS_rt_sigaction, signum, (uintptr_t) act, (uintptr_t) oldact, masksize);
 }
 
-static pid_t sys_wait4(
-	pid_t pid, int* wstatus, int options, struct rusage* rusage)
+pid_t sys_wait4(pid_t pid, int* wstatus, int options, rusage* rusage)
 {
 	return syscall4(SYS_wait4, pid, (uintptr_t) wstatus, options, (uintptr_t) rusage);
 }
 
-static ssize_t sys_write(int fd, const void* buf, size_t count)
+ssize_t sys_write(int fd, const void* buf, size_t count)
 {
 	return syscall3(SYS_write, fd, (uintptr_t) buf, count);
 }
 
-static void output(uint64_t i)
+void output(uint64_t i)
 {
 	if (sys_write(STDOUT_FILENO, &i, sizeof i) != sizeof i)
 		sys_exit(2);
 }
 
-static volatile uint64_t scan_addr;
-static volatile uint64_t handbase_addr;
+volatile uint64_t scan_addr;
+volatile uint64_t handbase_addr;
 
-static void segfault_handler(int signum UNUSED)
+void segfault_handler(int signum UNUSED)
 {
 	output(scan_addr);
 
@@ -91,8 +83,7 @@ static void segfault_handler(int signum UNUSED)
 	sys_exit(0);
 }
 
-NORETURN
-static void scan(uint64_t addr, uint64_t step)
+NORETURN void scan(uint64_t addr, uint64_t step)
 {
 	while (1) {
 #ifdef __amd64__
@@ -113,23 +104,21 @@ static void scan(uint64_t addr, uint64_t step)
 	}
 }
 
-int main(int argc UNUSED, char** argv UNUSED, char** envp)
+int inspect(uintptr_t init_addr)
 {
-	uint64_t init_addr = (uintptr_t) envp;
-
 	output(init_addr);
 
-	const struct sys_sigaction sa = {
+	const Sigaction sa = {
 		.handler = segfault_handler,
-		.flags = SYS_SA_RESTORER, // it is never needed
+		.flags = sa_restorer, // it is never needed
 		.restorer = nullptr,
 		.mask = {0},
 	};
 
-	if (sys_sigaction(SIGSEGV, &sa, NULL) != 0)
+	if (sys_sigaction(SIGSEGV, &sa, nullptr) != 0)
 		sys_exit(3);
 
-	if (sys_sigaction(SIGBUS, &sa, NULL) != 0)
+	if (sys_sigaction(SIGBUS, &sa, nullptr) != 0)
 		sys_exit(4);
 
 	pid_t pid = sys_fork();
@@ -141,11 +130,26 @@ int main(int argc UNUSED, char** argv UNUSED, char** envp)
 		sys_exit(5);
 	} else {
 		int status;
-		if (sys_wait4(-1, &status, 0, NULL) != pid)
+		if (sys_wait4(-1, &status, 0, nullptr) != pid)
 			sys_exit(6);
 		if (status != 0)
 			sys_exit(7);
 
 		scan(init_addr, sizeof(uint64_t));
 	}
+}
+
+} // namespace
+
+int main(int argc UNUSED, char** argv UNUSED, char** envp)
+{
+	// _start smuggles stack address as envp.
+	return inspect(uintptr_t(envp));
+}
+
+extern "C" void* memset(void* s, int c, size_t n)
+{
+	for (size_t i = 0; i < n; i++)
+		reinterpret_cast<uint8_t*>(s)[i] = c;
+	return s;
 }
