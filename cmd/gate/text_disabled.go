853bcc7077f4236dd31d69208ae6ade9337a1d64
Timo Savola <timo.savola@iki.fi>
2022-11-30 01:33:05 +0200

    Remove legacy build tags

--- cmd/gate/text_disabled.go
+++ cmd/gate/text_disabled.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 //go:build !gatedumptext
-// +build !gatedumptext
 
 package main
 
--- internal/container/child/debug_enabled.go
+++ internal/container/child/debug_enabled.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 //go:build gateruntimedebug
-// +build gateruntimedebug
 
 package child
 
